984af965c3a9dc019abcf68935e9edecdfa895cc
ttknpde-v <thitikorn_nupan@outlook.co.th>
2025-03-09 22:54:36 +0700

    add new module on abc-server*
    add module for connecting database h2 on abc-service and test on b-app module

--- b-app/b-web-controller/src/main/java/com/ttknp/bwebcontroller/web/controllers/BController.java
+++ b-app/b-web-controller/src/main/java/com/ttknp/bwebcontroller/web/controllers/BController.java
@@ -1,6 +1,8 @@
 package com.ttknp.bwebcontroller.web.controllers;
 
+import com.ttknp.abchelperconnectdatabaseh2.services.UserH2Service;
 import com.ttknp.bservicewebcontroller.webannotations.CommonRestAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,8 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @CommonRestAPI(value = "/b")
 public class BController {
 
+    private UserH2Service userH2Service;
+
+    @Autowired
+    public BController(UserH2Service userH2Service) {
+        this.userH2Service = userH2Service;
+    }
+
+
     @GetMapping(value = {"/","","/hello-world"})
     private @ResponseBody @ResponseStatus(HttpStatus.OK) String helloWorld() {
         return "Hello World Form B Web Controller!";
     }
+
+    @GetMapping(value = "/userH2")
+    private @ResponseBody @ResponseStatus(HttpStatus.OK) String userH2() {
+            userH2Service.testSelectAll();
+            return "Hello World Form B Web Controller!";
+    }
+
 }
